d4a183d9fec95ab43a7f46ee0b4e672cfe4be4a0
stalengd <p.zamorskiy@yandex.ru>
2024-09-24 20:05:19 +0300

    Added analog clock animation

--- Assets/Scripts/Clock/View/ClockViewAnalog.cs
+++ Assets/Scripts/Clock/View/ClockViewAnalog.cs
@@ -1,13 +1,19 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 namespace PustoStudio.ClockApp.Clock.View
 {
     public sealed class ClockViewAnalog : ClockViewBase
     {
+        [Header("Hands")]
         [SerializeField] private Transform _hourHand;
         [SerializeField] private Transform _minuteHand;
         [SerializeField] private Transform _secondHand;
+
+        [Header("Animation")]
+        [SerializeField] private float _tweenDurationSeconds = 0.5f;
+        [SerializeField] private Ease _tweenEase = Ease.InOutQuad;
 
         public override void SetTime(DateTime dateTime)
         {
@@ -17,9 +23,9 @@ namespace PustoStudio.ClockApp.Clock.View
             SetHand(_secondHand, GetSecondsAngle(time));
         }
 
-        private static void SetHand(Transform hand, float angle)
+        private void SetHand(Transform hand, float angle)
         {
-            hand.localRotation = Quaternion.Euler(0, 0, angle);
+            hand.DOLocalRotate(new(0, 0, angle), _tweenDurationSeconds).SetEase(_tweenEase);
         }
 
         private static float GetHoursAngle(TimeSpan time)
